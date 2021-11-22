138eb22b6fbaa4f08b96727716d9eb4c49b7a2f3
GokulVelavan <81863053+GokulVelavan@users.noreply.github.com>
2021-11-22 20:54:22 +0530

    uc3 handling custom exception

---
+++ MoodAnalyser/CustomException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    public class CustomException : Exception
+    {
+        public ExceptionType type;
+
+        
+        public enum ExceptionType //enum data type
+        {
+            NULL_TYPE_EXCEPTION,
+            EMPTY_TYPE_EXCEPTION,
+            CLASS_NOT_FOUND,
+            CONSTRUCTOR_NOT_FOUND
+
+        }
+
+        public CustomException(ExceptionType type, string message) : base(message) // constructor
+        {
+            this.type = type;
+        }
+    }
+}
