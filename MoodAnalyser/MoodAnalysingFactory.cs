e3b66a2c400816d8b109186a144a1d3e6d41dfbe
GokulVelavan <81863053+GokulVelavan@users.noreply.github.com>
2021-11-22 21:39:26 +0530

    mood analyser with default constructor

---
+++ MoodAnalyser/MoodAnalysingFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalysingFactory
+    {
+        public static object CreateMoodAnalyse(string className, string constructorName)
+        {
+            string pattern = "." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+
+                catch (Exception)
+                {
+                    throw new CustomException(CustomException.ExceptionType.CLASS_NOT_FOUND, "Class not found");
+                }
+            }
+            else
+            {
+                throw new CustomException(CustomException.ExceptionType.CONSTRUCTOR_NOT_FOUND, "Constructor not found");
+            }
+        }
+    }
+}
