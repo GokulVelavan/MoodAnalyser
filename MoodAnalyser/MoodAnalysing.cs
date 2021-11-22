eb0dfe6b119fdea70b73acc26fb88cb7a2d07082
GokulVelavan <81863053+GokulVelavan@users.noreply.github.com>
2021-11-22 20:16:26 +0530

    Add files via upload

---
+++ MoodAnalyser/MoodAnalysing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalysing
+    {
+        public string message;
+
+        public MoodAnalysing(string message) //constructor
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood() //program to find
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to mood analyser");
+            MoodAnalysing obj = new MoodAnalysing("I Am sad");
+            Console.WriteLine(obj.AnalyseMood());
+        }
+    }
+}
